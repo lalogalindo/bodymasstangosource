c14be3134d45667ecd8cd50d4f43e18d5535947f
Eduardo Galindo <eduardo.galindo@netgem.com>
2018-06-28 13:39:32 -0500

    Stashed file

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '52.41.154.187'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'bodymass_tangosource'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'soporte'),
+            'database' => env('DB_DATABASE', 'bodymass_test'),
+            'username' => env('DB_USERNAME', 'egalindo'),
+            'password' => env('DB_PASSWORD', 'eGalindo1.'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
