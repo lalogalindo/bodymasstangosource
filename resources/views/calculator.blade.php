2c5962f5c5ab61a313adb7f2c469b6787c58e28e
Eduardo Galindo <eduardo.galindo@netgem.com>
2018-06-28 17:11:18 -0500

    Data Bind between slider and input

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -16,32 +16,35 @@
           @guest
           In order to use the Body Mass Calculator, you must sign in.
           @else
-          <div id="input" class="col-md-8">
-            <p>Please, enter your height and weight.</p>
-            <div class="form-group">
-              <label for="height">Height</label>
-              <div class="row">
-                <div class="col-md-9">
-                  <input id="heightSlide" class="form-control" data-slider-id='heightSlider' type="text" data-slider-min="120" data-slider-max="210" data-slider-step="1" data-slider-value="170"/>
-                </div>
-                <div class="col-md-3">
-                  <input type="number" id="height" class="form-control">
-                </div>
+          <p>Please, enter your height and weight.</p>
+          <div class="form-group">
+            <label for="height">Height</label>
+            <div class="row">
+              <div class="col-md-8">
+                <input id="heightSlide" class="form-control" data-slider-id='heightSlider' type="text" data-slider-min="30" data-slider-max="250" data-slider-step="1" data-slider-value="170"/>
               </div>
-            </div>
-            <div class="form-group">
-              <label for="weight">Weight</label>
-              <div class="row">
-                <div class="col-md-9">
-                  <input id="weightSlide" class="form-control" data-slider-id='weightSlider' type="text" data-slider-min="120" data-slider-max="210" data-slider-step="1" data-slider-value="170"/>
-                </div>
-                <div class="col-md-3">
-                  <input type="number" id="weight" class="form-control">
+              <div class="col-md-4">
+                <div class="input-group">
+                  <input type="number" id="height" class="form-control" value="170">
+                  <span class="input-group-addon" id="basic-addon2">cm</span>
                 </div>
               </div>
             </div>
           </div>
-          <div id="image" class="col-md-4"></div>
+          <div class="form-group">
+            <label for="weight">Weight</label>
+            <div class="row">
+              <div class="col-md-8">
+                <input id="weightSlide" class="form-control" data-slider-id='weightSlider' type="text" data-slider-min="10" data-slider-max="200" data-slider-step="0.1" data-slider-value="170"/>
+              </div>
+              <div class="col-md-4">
+                <div class="input-group">
+                  <input type="number" id="weight" class="form-control" value="170">
+                  <span class="input-group-addon" id="basic-addon2">kg</span>
+                </div>
+              </div>
+            </div>
+          </div>
           @endguest
         </div>
       </div>
@@ -52,6 +55,13 @@
 
 @section('script')
 <script>
+$(document)
+.on('change','#heightSlide,#weightSlide',function(){
+  $(this).closest('.form-group').find('.input-group input').val(this.value)
+})
+.on('change','#height,#weight',function(){
+  $('#'+this.id+'Slide').slider('setValue',this.value,true)
+})
 $('#heightSlide').slider({
 	formatter: function(value) {
 		return 'Current value: ' + value;
